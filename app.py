f58a73731e4676ddf7e15b03b47b919cf974420a
Pacifique UMUGIRANEZA <pacifiquegiraneza12@gmail.com>
2024-12-14 13:55:12 +0200

    chatbot univeristy updated 14h00min

--- app.py
+++ app.py
@@ -35,7 +35,6 @@ def chatbot():
         if action == 'submit':  # When submitting a new question
             user_question = request.form['question']
             preprocessed_user_question = preprocess(user_question)
-            #Another lines of handling 
 
             # Combine user question with stored questions for vectorization
             corpus = [preprocessed_user_question] + list(processed_questions.values())
